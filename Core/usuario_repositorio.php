0426a54e22ebf0701b7819477f10d7557b91fadc
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-09-26 12:18:24 -0300

    foiii

--- Core/usuario_repositorio.php
+++ Core/usuario_repositorio.php
@@ -3,7 +3,7 @@ session_start();
 require_once '../include/funcoes.php';
 require_once 'conexao_mysql.php';
 require_once 'sql.php';
-require_once 'mysqli.php';
+require_once 'mysql.php';
 $salt = '$exemplosaltifsp';
 
 foreach ($_POST as $indice => $dado)
