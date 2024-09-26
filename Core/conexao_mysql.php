b185d77b127d23c660b7348d2d3d1d3a12754dd0
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-09-26 09:59:44 -0300

    foiii

--- Core/conexao_mysql.php
+++ Core/conexao_mysql.php
@@ -1,6 +1,25 @@
-<?php
+<?php           //Function CONECTA
 function conecta() : mysqli
 {
+    $servidor = 'localhost';
+    $banco    = 'Blog';
+    $port     = 3307;
+    $usuario  = 'root';
+    $senha    = '';
+    $conexao  = mysqli_connect($servidor, $usuario, $senha, $banco, $port);
 
+    if(!$conexao)
+    {
+        echo 'Erro: Não foi possível conectar ao MySql.' . PHP_EOL;
+        echo 'Debugging errno: ' . mysqli_connect_errno() . PHP_EOL;
+        echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+        return null;
+    }
+    return $conexao;
+}
+            //Function DESCONECTA
+function desconecta($conexao)
+{
+    mysqli_close($conexao);
 }
 ?>
