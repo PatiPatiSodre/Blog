ad68c3451f5709fd2662fbef24c1ac879e6ecb9b
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-10-03 12:27:02 -0300

    foiii

---
+++ post_detalhe.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+    require_once 'include/funcoes.php';
+    require_once 'Core/conexao_mysql.php';
+    require_once 'Core/sql.php';
+    require_once 'Core/mysql.php';
+
+    foreach ($_GET as $indice => $dado)
+    {
+        $$indice = LimparDados($dado);
+    }
+
+    $posts = buscar(
+        'post',
+        [
+            'titulo',
+            'data_postagem',
+            'testo',
+            '(select nome
+                from usuario
+                where usuario.id = post.usuario_id) as nome'
+        ],
+    )
+?>
