98218f73d5642e9d44de0003418b1df6d8034988
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-10-03 09:29:34 -0300

    foiii

--- Banco/banco.sql
+++ Banco/banco.sql
@@ -1,21 +1,21 @@
 Create Table usuario
-{
-    id           int NOT NULL AUTO_INCREMET,
+(
+    id           int NOT NULL AUTO_INCREMENT,
     nome         VARCHAR(050) NOT NULL,
     email        VARCHAR(255) NOT  NULL,
     senha        VARCHAR(060) NOT NULL,
-    data_criacao datetime NOT NULL DEFAULT CURRNT_TIMESTAMP,
+    data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     ativo        tinyint NOT NULL DEFAULT '0',
-    adm          tinyint NOT NULL DEFAULT '0',
+    adm          tinyint NOt NULL DEFAULT '0',
     PRIMARY KEY (id)
-};
+);
 
 Create Table post (
-    id            int NOT NULL AUTO_INCREMET,
+    id            int NOT NULL AUTO_INCREMENT,
     titulo        VARCHAR(255) NOT NULL,
     texto         text NOT NULL,
     usuario_id    int NOT NULL,
-    data_criacao  datetime NOT NULL DEFAULT CURRNT_TIMESTAMP,
+    data_criacao  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     data_postagem datetime NOT NULL,
     PRIMARY KEY (id),
     KEY        fk_post_usuario_idx (usuario_id),
