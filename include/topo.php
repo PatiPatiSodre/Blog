643562e93de4c0385ccd962fcd186ddb9eff7a04
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-10-03 10:50:23 -0300

    foiii

--- include/topo.php
+++ include/topo.php
@@ -2,18 +2,16 @@
     session_start();
 ?>
 
-
 <div class="card">
     <div class="card-header">
         <h1> Projeto Blog em PHP + MySQL IFSP - Patrícia</h1>
 </div>
 
-
 <?php if(isset($_SESSION['login'])) : ?>
     <div class="card-body text-right">
         Olá <?php echo $_SESSION['login']['usuario']['nome']?>!
         <a  href="Core/usuario_repositorio.php?acao=logout"
-            class="btn-link btn-sm" role="button">Sair</a>
+            class="btn btn-link btn-sm" role="button">Sair</a>
     </div>
     <?php endif ?>
 </div>
