aa6210079e2c219d3f24cced26f3599971c50c52
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-10-24 10:19:41 -0300

    foi

--- Core/post_repositorio.php
+++ Core/post_repositorio.php
@@ -2,7 +2,7 @@
 session_start();
 require_once '../include/valida_login.php';
 require_once '../include/funcoes.php';
-require_once 'conexao_mysql.php';
+require_once 'conexao_mysql.php';               // inclui os arquivos
 require_once 'sql.php';
 require_once 'mysql.php';
 
@@ -10,7 +10,7 @@ foreach($_POST as $indice => $dado)
 {
     $$indice = LimparDados($dado);
 }
-
+                        
 foreach($_GET as $indice => $dado)
 {
     $$indice = LimparDados($dado);
@@ -65,6 +65,6 @@ switch($acao)
 
             break;
 }
-
+    // volta  para a pagina index
 header('Location: ../index.php');
 ?>
